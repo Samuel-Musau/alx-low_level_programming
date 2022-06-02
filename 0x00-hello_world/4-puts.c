f794fa6923e9e4585f57b6a127f0e0fa642281c0
Samuel-Musau <musausamuel49@gmail.com>
2022-06-02 15:11:33 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - my entry point
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
