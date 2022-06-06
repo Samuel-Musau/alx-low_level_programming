26b4198075fbd343cb6cca674b76472b0f24bd51
Samuel-Musau <musausamuel49@gmail.com>
2022-06-06 10:24:32 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,10 +9,9 @@ int main(void)
 {
 		int digit;
 
-		for (digit = 0; digit < 10; digit++)
-		
-			printf("%i", digit);
-			putchar('\n');
-		
-		return (0);
+			for (digit = 0; digit < 10; digit++)
+					printf("%i", digit);
+				putchar('\n');
+
+					return (0);
 }
